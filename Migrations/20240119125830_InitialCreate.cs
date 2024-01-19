6e051d97f403c3e15a563f4d747e94909835802c
HolyD2ck <petranatolevic102@gmail.com>
2024-01-19 21:54:02 +0800

    123

---
+++ Migrations/20240119125830_InitialCreate.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Prepod",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Фамилия = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Имя = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Отчество = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Куратор_Группы = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Профессия = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    День_Рождения = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Номер_Кабинета = table.Column<int>(type: "int", nullable: false),
+                    Зарплата = table.Column<int>(type: "int", nullable: false),
+                    Стаж = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Prepod", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Student",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Фамилия = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Имя = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Отчество = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Рост = table.Column<int>(type: "int", nullable: false),
+                    День_Рождения = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Группа = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Специальность = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Стипендия = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Возраст = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Student", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "View",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Фамилия = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Имя = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Отчество = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Рост = table.Column<int>(type: "int", nullable: false),
+                    День_Рождения = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Группа = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Специальность = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Стипендия = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Возраст = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_View", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Prepod");
+
+            migrationBuilder.DropTable(
+                name: "Student");
+
+            migrationBuilder.DropTable(
+                name: "View");
+        }
+    }
+}
--- Models/Student.cs
+++ Models/Student.cs
@@ -1,4 +1,6 @@
-﻿namespace Data.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Data.Models
 {
     public class Student
     {
@@ -10,6 +12,35 @@
         public string? День_Рождения { get; set; }
         public string? Группа { get; set; }
         public string? Специальность { get; set; }
+        public string? Стипендия { get; set; } 
+        public int? Возраст { get; set; }
+    }
+    public class View
+    {
+        public int id { get; set; }
+        public string? Фамилия { get; set; }
+        public string? Имя { get; set; }
+        public string? Отчество { get; set; }
+        public int Рост { get; set; }
+        public string? День_Рождения { get; set; }
+        public string? Группа { get; set; }
+        public string? Специальность { get; set; }
         public string? Стипендия { get; set; }
+        public int? Возраст { get; set; }
+    }
+    public class Prepod
+    {
+        public int id { get; set; }
+        public string? Фамилия { get; set; }
+        public string? Имя { get; set; }
+        public string? Отчество { get; set; }
+        public string? Куратор_Группы { get; set; }
+        public string? Профессия { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime День_Рождения { get; set; }
+        public int Номер_Кабинета { get; set; }
+        public int Зарплата { get; set; }
+        public int Стаж { get; set; }
     }
 }
